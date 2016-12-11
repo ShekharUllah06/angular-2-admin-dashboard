43b16d2d792d411b5c04e8be2a6a8a684d3942ef
Shekhar Ullah <ullah.shekhar@gmail.com>
2016-12-11 14:49:19 +0600

    top menu added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './components/top-menu/top-menu.component';
@@ -14,7 +15,8 @@ import { TopMenuComponent } from './components/top-menu/top-menu.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/top-menu/top-menu.component.ts
+++ src/app/components/top-menu/top-menu.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ROUTES } from './top-menu-routes.config';
+import { MenuType } from './top-menu.metadata';
 @Component({
-  selector: 'app-top-menu',
+  selector: 'top-menu',
   templateUrl: './top-menu.component.html',
   styleUrls: ['./top-menu.component.scss']
 })
 export class TopMenuComponent implements OnInit {
 
-  constructor() { }
+  public menuItems: any[];
+  public brandMenu: any;
+  isCollapsed = true;
+
+  constructor() {}
 
   ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem.menuType !== MenuType.BRAND);
+    this.brandMenu = ROUTES.filter(menuItem => menuItem.menuType === MenuType.BRAND)[0];
+  }
+
+  public get menuIcon(): string {
+    return this.isCollapsed ? '☰' : '✖';
+  }
+
+  public getMenuItemClasses(menuItem: any) {
+    return {
+      'pull-xs-right': this.isCollapsed && menuItem.menuType === MenuType.RIGHT
+    };
   }
 
 }
